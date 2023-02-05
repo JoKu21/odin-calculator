ade5a4393444670b4c45ec0774d8d9af4de756c7
Johannes <J-Bird@gmx.de>
2023-02-05 17:42:41 +0100

    Create initial files
    1. Create index.html, styles.css, calc.js
    2. Build index.html
    3. Start css
    4. Implement simple math functions

---
+++ calc.js
@@ -0,0 +1,31 @@
+//Declaration of Test-Variables
+const a = 5;
+const b = 10;
+
+//add-function
+function sum(a,b){
+    return a + b;
+}
+
+//subtract-function
+function sub(a,b){
+    return a - b;
+}
+
+//multiply-function
+function multi(a,b){
+    return a * b;
+}
+
+//divide-function
+function div(a,b){
+    return a / b;
+}
+
+//Test
+console.log(sum(a,b));
+console.log(sub(a,b));
+console.log(multi(a,b));
+console.log(div(a,b));
+
+//function operate -> 

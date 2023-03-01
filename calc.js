2184c1cb87f4f8c94e48ebd853a77dbed0a04f20
Johannes <J-Bird@gmx.de>
2023-03-01 20:08:52 +0100

    Update js-File

--- calc.js
+++ calc.js
@@ -24,8 +24,8 @@ const displayOutput = document.getElementById('outputField');
 
 //additional declaration
 let helpVar = '';
-let a = 5;
-let b = 3
+let a = 0;
+let b = 0;
 let operator = '';
 
 //functions
@@ -46,26 +46,37 @@ function div(a,b) {
     return a / b;
 }
 
-function operate(a,b,operator) {
-    switch(operator) {
-        case '+':
-            sum(a,b)
-            break;
+function operate(a,b,sign) {
 
-        case '-':
-            sub(a,b)
-            break;
+    if(a === 0){
+       a = Number(displayOutput.textContent);
+       
+       console.log(`var a = ${a}`);
+       displayOutput.textContent = '';
+        //Mach das nächste Mal hier weiter. Die globalen Variablen müssen fest geändert und immer wieder abgerufen werden. Damit kann dann darauf zugegriffen werden und sie können mit dem Operator verknüpft werden
+    } else {
+        b = Number(displayOutput.textContent);
+        console.log(`b = ${b}, operator = ${sign}`);
+        switch(sign) {
+            case '+':
+                helpVar = sum(a,b)
+                break;
 
-        case '*':
-            multi(a,b)
-            break;
+            case '-':
+                helpVar = sub(a,b)
+                break;
 
-        case'/':
-            div(a,b)
-            break;
+            case '*':
+                helpVar = multi(a,b)
+                break;
 
-        default:
-            return 'no legal operation' 
+            case'/':
+                helpVar = div(a,b)
+                break;
+
+            default:
+                return helpVar 
+        }
     }
 }
 
@@ -73,6 +84,20 @@ function addSign(num) {
     let x = displayOutput.textContent;
     x += num;
     return displayOutput.textContent = x;
+}
+
+function removeSign() {
+    let y = displayOutput.textContent.toString();
+    let help = y.slice(0,y.length-1);
+    return displayOutput.textContent = Number(help);
+}
+
+function clear() {
+    a = 0;
+    b = 0;
+    displayOutput.textContent = '';
+    operator = '';
+    helpVar = '';
 }
 
 //Add functions to buttons
@@ -87,4 +112,13 @@ button7.addEventListener('click',() => {addSign('7');});
 button8.addEventListener('click',() => {addSign('8');});
 button9.addEventListener('click',() => {addSign('9');});
 buttonDot.addEventListener('click',() => {addSign('.');});
+
+buttonBack.addEventListener('click',() => {removeSign();});
+buttonClear.addEventListener('click',() => {clear();});
+buttonResult.addEventListener('click',() => {result();});
+
+buttonSum.addEventListener('click',() => {operate(a,b,'+');});
+buttonSub.addEventListener('click',() => {operate(a,b,'-');});
+buttonMulti.addEventListener('click',() => {operate(a,b,'*');});
+buttonDiv.addEventListener('click',() => {operate(a,b,'/');});
 
